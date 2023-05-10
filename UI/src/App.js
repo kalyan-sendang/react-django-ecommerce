f4d249155e3a7ec06f54cf500224a9874b120de4
SoulK <58691148+kalyan-sendang@users.noreply.github.com>
2023-05-11 00:38:44 +0545

    Add files via upload

---
+++ UI/src/App.js
@@ -0,0 +1,44 @@
+
+import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+import CartScreen from './screens/CartScreen'
+import LoginScreen from './screens/LoginScreen'
+import RegisterScreen from './screens/RegisterScreen'
+import ProfileScreen from './screens/ProfileScreen'
+import ShippingScreen from './screens/ShippingScreen'
+import PaymentScreen from './screens/PaymentScreen'
+import PlaceOrderScreen from './screens/PlaceOrderScreen'
+import OrderScreen from './screens/OrderScreen'
+
+
+function App() {
+  return (
+    <Router>
+      <Header />
+        <main className='py-3'>
+          <Container>
+            <Routes>
+            <Route exact path ='/' element={<HomeScreen/>}  />
+            <Route path ='/register' element={<RegisterScreen/>}  />
+            <Route path ='/login' element={<LoginScreen/>}  />
+            <Route path ='/profile' element={<ProfileScreen/>}  />
+            <Route path ='/shipping' element={<ShippingScreen/>}  />
+            <Route path ='/payment' element={<PaymentScreen/>}  />
+            <Route path ='/placeorder' element={<PlaceOrderScreen/>}  />
+            <Route path ='/order/:id' element={<OrderScreen/>}  />
+            <Route path ='/product/:id' element={<ProductScreen/>}  />
+            <Route path ='/cart/:id' element={<CartScreen/>}  />
+            <Route path ='/cart' element={<CartScreen/>}  />
+            </Routes>
+          </Container>
+        </main>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
