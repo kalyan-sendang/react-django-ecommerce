ea00fcba0ac662131f033d1ed89f618eb5a6c920
SoulK <58691148+kalyan-sendang@users.noreply.github.com>
2023-06-18 23:12:51 +0545

    Delete backend directory

--- backend/base/urls/order_urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import URLPattern, path
-from base.views import order_views as views
-
-
-urlpatterns = [
-    path('add/', views.addOrderItems, name='orders-add'),
-    path('<str:pk>/', views.getOrderById, name='user-order'),
-    path('<str:pk>/pay/', views.updateOrderPaidTo, name='pay'),
-]
